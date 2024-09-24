c3e37c490046b1a237adc57d6ebbf738ae6254c4
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2024-09-24 03:03:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,20 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/muhammad-badran/ml-penguins-classifier/refs/heads/main/penguins.csv')
   df
-st.write('The inputs features (X)')
-X = df.drop('species', axis=1)
-X
 
-st.write('The output vector (y)')
-y = df.species
-y
+  st.write('The inputs features (X)')
+  X = df.drop('species', axis=1)
+  X
 
+  st.write('The output vector (y)')
+  y = df.species
+  y
+
+with st.expander('Data Visualization'):
+  st.scatter_chart(
+    data=df,
+    X='bill_length_mm',
+    y='body_mass_g',
+    color='species'
+  )
+    
