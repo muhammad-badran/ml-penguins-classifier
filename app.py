cf0d0de61f4dccc156ecb636cffb2fcf7371c96d
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2024-09-24 02:09:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.title('Machine Learning App to Classify Penguins')
 
 st.info('This is a mahcine learning app that builds a model to predict the spieces of penguins.')
 
-df = pd.read_csv('https://https://raw.githubusercontent.com/muhammad-badran/ml-penguins-classifier/refs/heads/main/penguins.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/muhammad-badran/ml-penguins-classifier/refs/heads/main/penguins.csv')
+df
 
 st.write('Hello world!')
