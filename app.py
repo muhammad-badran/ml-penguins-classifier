65cf38c793e513fac576b8c3ecc779ce46a672df
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2024-09-24 02:09:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning App to Classify Penguins')
 
 st.info('This is a mahcine learning app that builds a model to predict the spieces of penguins.')
+
+df = pd.read_csv('https://https://raw.githubusercontent.com/muhammad-badran/ml-penguins-classifier/refs/heads/main/penguins.csv')
 
 st.write('Hello world!')
