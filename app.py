0e9ea97ef56b98a50d54c63b520f4761f4eb88b5
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2024-09-24 02:05:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title(':robot: Machine Learning App to Classify Penguins')
+# st.title('Machine Learning App to Classify Penguins')
 
 st.info('This is a mahcine learning app that builds a model to predict the spieces of penguins.')
 
