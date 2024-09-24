6ad9c3c2c2c18b431c4170a46a0d4a5435a9b952
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2024-09-24 05:08:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ with st.expander('Input Features'):
 
 # Encode categorical data
 encode = ['island', 'sex']
-df_penguins = pd.get_dummies(input_penguins prefix=encode)
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
