62cf5bedf4a00b886483d0fb3bb94c1153150e23
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2024-09-24 02:13:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/muhammad-badran/ml-penguins-classifier/refs/heads/main/penguins.csv')
   df
-st.write('The inputs features (X)'):
+st.write('The inputs features (X)')
 X = df.drop('species' axis=1)
 X
 
