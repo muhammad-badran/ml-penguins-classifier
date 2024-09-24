3ae042c478dd17e42a776bc5d65e8d6f2841c78c
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2024-09-24 08:24:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 st.title('Machine Learning App: Penguins Species Prediction')
 
